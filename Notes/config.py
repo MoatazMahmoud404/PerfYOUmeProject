d07ad212cfabbbef3fa936dd34c159751856125c
mahmoudnaif <mahmoudnaif788@gmail.com>
2024-12-06 13:26:32 +0200

    Added compatible DB verstion and .ignore modifcications
    Added the cache files to the .ignore folder and moved the conf exapmple the notes folder each dev must move the the config outside of this subfolder and edits his connection string

---
+++ Notes/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+SQLALCHEMY_DATABASE_URI = (
+     'mssql+pyodbc://(LocalDB)\\MSSQLLocalDB/PerfYOUmeDatabase?'
+    'driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+)
--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-SQLALCHEMY_DATABASE_URI = (
-    'mssql+pyodbc://@Moataz404Mahmoud\\SQLEXPRESS/PerfYOUmeDatabase?'
-    'driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
-)
